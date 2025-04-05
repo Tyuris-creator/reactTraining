1f58dc358b24b265b793c0965b2ef0087c4dc95d
Michail Tiurin <tyuris@icloud.com>
2025-04-05 15:21:07 +0300

    more

--- Project15/react-app/src/MyComponent.jsx
+++ Project15/react-app/src/MyComponent.jsx
@@ -17,10 +17,54 @@
 // #5 Clean uo when a component unmounts
 
 import React, {useState} from "react";
+import { useEffect } from "react";
 
 function MyComponent(){
+
+    const [count, setCount] = useState(0)
+    const [color, setColor] = useState("green")
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight)
+
+
+    useEffect(()=>{
+        document.title = `Count: ${count} ${color} Size: ${width} x ${height} px`;
+        window.addEventListener("resize", handleResize)
+        console.log("added")
+
+        return ()=>{
+            window.removeEventListener("resize", handleResize)
+            console.log("removed")
+        }
+
+    }, [count, color, width, height])
+
+    function addCount(){
+        setCount(prevCount => prevCount+1)
+    }
+
+    function substractCount(){
+        setCount(prevCount => prevCount-1)
+    }
+
+    function changeColor(){
+        setColor(c => c === "green" ? "red" : "green")
+    }
+
+    function handleResize(){
+        setWidth(window.innerWidth)
+        setHeight(window.innerHeight)
+    }
+
     return (
-        <p>hui</p>
+        <>
+            <p style={{color: color}}>Count: {count}</p>
+            <button onClick={addCount}>Add</button>
+            <button onClick={substractCount}>Substract</button><br></br>
+            <button onClick={changeColor}>Change Color</button><br></br>
+            <p>Window Width: {width}px</p>
+            <p>Window Height: {height}px</p>
+        </>  
     )
 }
 
